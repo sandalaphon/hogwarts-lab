09b8088acc0262b6ecdfe3dfb1c2c05be1c06de9
Richard Page-Croft <rpcroft@gmail.com>
2017-02-22 14:36:45 +0000

    update, delete etc added to Student class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require_relative('../models/student')
 require('pry')
 
+Student.delete_all
+
 @student1 = Student.new ({
   "first_name" => "Harry",
   "last_name" => "Potter",
--- models/student.rb
+++ models/student.rb
@@ -20,5 +20,39 @@ class Student
     sql = "INSERT INTO students (first_name, last_name, house, age) VALUES ( '#{@first_name}', '#{@last_name}', '#{@house}' , '#{@age}') RETURNING *;"
     @id = SqlRunner.run(sql).first()['id'].to_i
   end 
+  def update()
+    sql = "UPDATE students SET
+      first_name = '#{ @first_name }',
+      last_name = '#{ @last_name }',
+      house = '#{ @house }',
+      age = '#{ @age }'
+      WHERE id = '#{ @id }';"
+    SqlRunner.run( sql )
+  end
+
+  def delete()
+    sql = "DELETE FROM students WHERE id=#{ @id };"
+    SqlRunner.run( sql )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students;"
+    students = SqlRunner.run( sql )
+    result = students.map { |student| Student.new( student ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM students WHERE id=#{id};"
+    student = SqlRunner.run( sql )
+    result = Student.new( student.first )
+
+    return result
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM students;"
+    SqlRunner.run(sql)
+  end
 
 end
